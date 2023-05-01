8e55879dd630d4593ebe025247e3db41fb18e8a2
OzanOcak <ozanocak@gmail.com>
2023-05-01 14:10:13 -0400

    json file

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -5,10 +5,52 @@ const LOCAL_STORAGE_KEY = "todo_list";
 export default function useLocalStorage() {
   const [posts, setPosts] = useState([
     {
-      id: 99,
-      title: "screw up",
+      id: 21,
+      title: "ask somebody out",
       datetime: "July 01, 2021 11:17:36 AM",
       body: "inivite on a date",
+    },
+    {
+      id: 22,
+      title: "ask around",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "ask many people the same question",
+    },
+    {
+      id: 23,
+      title: "add up to something",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "sum up, equal",
+    },
+    {
+      id: 24,
+      title: "back something up",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "withdraw, reverse",
+    },
+    {
+      id: 25,
+      title: "break down",
+      datetime: "July 03, 2021 11:17:36 AM",
+      body: "stop functioning",
+    },
+    {
+      id: 26,
+      title: "break in",
+      datetime: "July 04, 2021 11:17:36 AM",
+      body: "interrupt",
+    },
+    {
+      id: 27,
+      title: "blow up",
+      datetime: "July 05, 2021 11:17:36 AM",
+      body: "explode",
+    },
+    {
+      id: 28,
+      title: "back somebody up",
+      datetime: "July 06, 2021 11:17:36 AM",
+      body: "stand with, support",
     },
   ]);
 
@@ -30,55 +72,3 @@ export default function useLocalStorage() {
 
   return { posts, setPostsAndSave };
 }
-
-/**
- *     {
-      id: 1,
-      title: "ask somebody out",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "inivite on a date",
-    },
-    {
-      id: 2,
-      title: "ask around",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "ask many people the same question",
-    },
-    {
-      id: 3,
-      title: "add up to something",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "sum up, equal",
-    },
-    {
-      id: 4,
-      title: "back something up",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "withdraw, reverse",
-    },
-    {
-      id: 5,
-      title: "break down",
-      datetime: "July 03, 2021 11:17:36 AM",
-      body: "stop functioning",
-    },
-    {
-      id: 6,
-      title: "break in",
-      datetime: "July 04, 2021 11:17:36 AM",
-      body: "interrupt",
-    },
-    {
-      id: 7,
-      title: "blow up",
-      datetime: "July 05, 2021 11:17:36 AM",
-      body: "explode",
-    },
-    {
-      id: 8,
-      title: "back somebody up",
-      datetime: "July 06, 2021 11:17:36 AM",
-      body: "stand with, support",
-    },
- * 
- */
